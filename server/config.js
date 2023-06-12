92d600069ab5bcf87b689c46832b758c3fe93077
Misash <misash@outlook.com>
2023-06-11 19:41:38 -0500

    update config.js

--- server/config.js
+++ server/config.js
@@ -6,3 +6,10 @@ export const API_KEY = "418231572151953"
 export const API_SECRET = "BK7LIxeRQOL2lvm14L5Zk5pqiLE"
 
 
+//database
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || '1234'
+export const DB_NAME = process.env.DB_NAME || 'startjunior'
+export const DB_PORT = process.env.DB_PORT || 3306
+
--- server/db.js
+++ server/db.js
@@ -1,10 +1,17 @@
 import {createPool} from "mysql2/promise"
+import {
+    DB_HOST,
+    DB_PORT,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME
+}from './config.js'
 
 export const pool = new createPool({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "1234",
-    database: "startjunior"
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
 })
 
